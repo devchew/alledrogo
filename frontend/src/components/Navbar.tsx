83c7d76318d61df513bba922a66d1ae2908cfaed
DevChew <the@devchew.com>
2023-05-27 16:07:27 +0200

    cleanup navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -16,20 +16,16 @@ const Navbar: FunctionComponent = () => {
     <div className="navbar">
       <NavLink to="/" className="navbar__branding">Alledrogo</NavLink>
       <div className="navbar__links">
-        <NavLink to="/auctions" className={navLinkStyle}>Aukcje</NavLink>
-        <NavLink to="/auctions" className={navLinkStyle}>Inne aukcje</NavLink>
-        <NavLink to="/auctions" className={navLinkStyle}>Jeszcze inne aukcje</NavLink>
+        <IfAuth ifNot>
+          <NavLink to="/register" className={navLinkStyle}>Rejestracja</NavLink>
+          <NavLink to="/login" className={navLinkStyle}>Zaloguj</NavLink>
+        </IfAuth>
+        <IfAuth>
+          <NavLink to="/profile" className={navLinkStyleButton}>Konto</NavLink>
+          <NavLink to="/auction/add" className={navLinkStyleButton}>Dodaj +</NavLink>
+          <button type="button" onClick={logout} className="navbar__link">Wyloguj się</button>
+        </IfAuth>
       </div>
-
-      <IfAuth ifNot>
-        <NavLink to="/register" className={navLinkStyle}>Rejestracja</NavLink>
-        <NavLink to="/login" className={navLinkStyle}>Zaloguj</NavLink>
-      </IfAuth>
-      <IfAuth>
-        <NavLink to="/profile" className={navLinkStyleButton}>Konto</NavLink>
-        <NavLink to="/auction/add" className={navLinkStyleButton}>Dodaj +</NavLink>
-        <button type="button" onClick={logout} className="navbar__link">Wyloguj się</button>
-      </IfAuth>
     </div>
   );
 };
